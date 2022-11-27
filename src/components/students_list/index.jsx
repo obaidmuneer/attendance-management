f979e8d6482d5ee356f5f33aa9e70cd396289c99
obaidmuneer <obaidmuneer@gmail.com>
2022-11-27 13:08:25 +0500

    created std list comp

---
+++ src/components/students_list/index.jsx
@@ -0,0 +1,42 @@
+
+const StudentsList = ({ students }) => {
+    return (
+        <div>
+            {
+                students ?
+                    <table>
+                        <thead>
+                            {
+                                students[0] && <tr>
+                                    <td>{Object.keys(students[0])[1]}</td>
+                                    <td>{Object.keys(students[0])[2]}</td>
+                                    <td>{Object.keys(students[0])[3]}</td>
+                                    <td>{Object.keys(students[0])[4]}</td>
+                                    {/* <td>{Object.keys(students[0])[5]}</td> */}
+                                    <td>{Object.keys(students[0])[6]}</td>
+                                </tr>
+                            }
+                        </thead>
+                        <tbody>
+                            {
+                                students.map((std, index) => {
+                                    return <tr key={index} >
+                                        <td>{std.name}</td>
+                                        <td>{std.fathername}</td>
+                                        <td>{std.roll}</td>
+                                        <td>{std.contact}</td>
+                                        {/* <td>{std.cnic}</td> */}
+                                        <td>{std.course}</td>
+                                    </tr>
+                                })
+                            }
+                        </tbody>
+                    </table>
+                    :
+                    <div>Something went wrong</div>
+            }
+        </div>
+    )
+}
+
+export default StudentsList
