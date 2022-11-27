922cae13808da7320537b5a4e47be5917ef4af8c
obaidmuneer <obaidmuneer@gmail.com>
2022-11-27 13:14:48 +0500

    some changes

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './App.css';
 import Home from './components/home';
 import Student from './components/student';
 import Attendance from './components/attendance';
+import AddStudent from './components/add_student';
 
 let api = 'http://localhost:8080'
 
@@ -15,13 +16,14 @@ const App = () => {
 
         <ul>
           <li><Link to={'/'} >Home</Link></li>
-          <li><Link to={'/student'} >Student</Link></li>
+          <li><Link to={'student'} >Student</Link></li>
           <li><Link to={'attendance'} >Attendance</Link></li>
         </ul>
         <Routes>
           <Route path='/' element={<Home api={api} />} />
-          <Route path='/student' element={<Student api={api} />} />
-          <Route path='/attendance' element={<Attendance api={api} />} />
+          <Route path='student' element={<Student api={api} />} />
+          <Route path='add_student' element={<AddStudent api={api} />} />
+          <Route path='attendance' element={<Attendance api={api} />} />
         </Routes>
       </div>
     </div>
