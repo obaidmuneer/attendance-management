cbe608ddf179e8f777db391f20d5ed6e0c08c1b7
obaidmuneer <obaidmuneer@gmail.com>
2022-11-27 13:09:26 +0500

    opt to approve std

--- src/components/dasboard/index.jsx
+++ src/components/dasboard/index.jsx
@@ -1,21 +1,40 @@
 import { useEffect, useState } from 'react'
 import axios from 'axios'
+import { Link } from 'react-router-dom'
 
-const Dashboard = () => {
+
+const Dashboard = ({ api }) => {
     const [students, setStudents] = useState([])
 
     useEffect(() => {
-        axios.get('http://localhost:8080/students')
+        axios.get(`${api}/students`)
             .then(res => setStudents(res.data.data))
         console.log(students);
         // eslint-disable-next-line
     }, [])
+
+    const approveStudent = (std) => {
+        // console.log(std);
+        axios.post(`${api}/enroll_student`, {
+            course: std.course,
+            roll: std.roll
+        })
+        let data = students.filter((eachStd,index) => {
+            return eachStd.roll !== std.roll
+        })
+        // console.log(data);
+        setStudents(data)
+
+    }
     return (
         <div>
             <h3>Dashbord</h3>
+
+            <Link to={'add_student'} >Add Student</Link>
             {
                 students.map((std, index) => {
-                    return !std.isClassAssign && <div key={index}>{std.name} applied for {std.course} course</div>
+                    return !std.isClassAssign && <div key={index}>{std.name} applied for {std.course} course
+                        <input type="button" value="Approve" onClick={() => approveStudent(std)} /> </div>
                 })
             }
         </div>
