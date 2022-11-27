8b9912f2276b84a28c9743c7b1caff2ee90f41c5
obaidmuneer <obaidmuneer@gmail.com>
2022-11-27 13:08:44 +0500

    fetched all std

--- src/components/student/index.jsx
+++ src/components/student/index.jsx
@@ -1,7 +1,21 @@
+import StudentsList from '../students_list'
+import axios from "axios"
+import { useEffect, useState } from "react"
 
-const Student = () => {
+const Student = ({ api }) => {
+  const [students, setStudents] = useState([])
+  useEffect(() => {
+    axios.get(`${api}/students`)
+      .then(res => setStudents(res.data.data))
+      .catch(err => setStudents(null))
+
+    // eslint-disable-next-line
+  }, [])
+
   return (
-    <div>Student</div>
+    <div>
+      <StudentsList students={students} />
+    </div>
   )
 }
 
