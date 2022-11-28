bb221d846a98890cf93dac208e37d110cf255f84
obaidmuneer <obaidmuneer@gmail.com>
2022-11-28 23:04:30 +0500

    add opt to add img

--- src/components/add_student/index.jsx
+++ src/components/add_student/index.jsx
@@ -8,28 +8,55 @@ const AddStudent = ({ api }) => {
     const [contact, setContact] = useState('')
     const [cnic, setCnic] = useState('')
     const [course, setCourse] = useState('')
+    const [image, setImage] = useState({ preview: '', data: '' })
 
     const navigate = useNavigate()
 
-    const handleSubmit = (e) => {
+    const handleSubmit = async(e) => {
         e.preventDefault()
-        axios.post(`${api}/students`, {
+
+        let formData = new FormData();
+        formData.append('pic', image.data)
+        // console.log(formData);
+
+       const result = await axios.post(`${api}/students`, {
             name,
             fathername,
             contact,
             cnic,
             course //make the list to be fethced from server
         })
-            .then(res => console.log(res.data))
-            .catch(err => console.log(err))
+        console.log(result.data.data.roll);
+
+        axios.post(`${api}/students/img/${result.data.data.roll}`, formData,
+            {
+                headers: {
+                    'Content-Type': 'multipart/form-data'
+                }
+            })
+            .then((res) => {
+                console.log(res.data);
+            })
+            .catch((err) => {
+                console.log(err);
+            })
+
         navigate('/')
 
+    }
+
+    const handleFileChange = (e) => {
+        const img = {
+            preview: URL.createObjectURL(e.target.files[0]),
+            data: e.target.files[0]
+        }
+        setImage(img)
     }
 
     return (
         <div>
             <h1>Add Student</h1>
-            <form onSubmit={handleSubmit}>
+            <form encType="multipart/form-data" onSubmit={handleSubmit}>
                 <span>Name :</span> <input type="text" onChange={(e) => setName(e.target.value)} value={name} /> <br />
                 <span>Father Name :</span> <input type="text" onChange={(e) => setFathername(e.target.value)} value={fathername} /> <br />
                 <span>Contact No:</span> <input type="number" onChange={(e) => setContact(+e.target.value)} value={contact} /> <br />
@@ -37,9 +64,11 @@ const AddStudent = ({ api }) => {
                 <span>Course :</span>
                 <select defaultValue={"course"} onChange={(e) => setCourse(e.target.value)}>
                     <option value="course" disabled >Select Course</option>
+                    <option value="chatbot">Chatbot</option>
                     <option value="web">WEB</option>
                     <option value="graphic">Graphic</option>
                 </select> <br />
+                <input type="file" name="pic" onChange={handleFileChange} />
                 <input type="submit" value="Add Student" />
             </form>
         </div>
