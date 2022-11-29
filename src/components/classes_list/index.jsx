436874a01868e80236123c0552ffcff7827049a2
obaidmuneer <obaidmuneer@gmail.com>
2022-11-29 23:04:33 +0500

    updated comp

--- src/components/classes_list/index.jsx
+++ src/components/classes_list/index.jsx
@@ -1,31 +1,50 @@
-import { useEffect, useState } from "react"
+import axios from "axios"
+import { useState } from "react"
 
 
-const ClassesList = ({ stdCourse, classes, defaultVal, fetchedVal }) => {
-    const [selectedClass, setSelectedClass] = useState([])
+const ClassesList = ({ api, std, deLoad }) => {
+    const [classData, setClassData] = useState([])
+    const [selectedBatch, setSelectedBatch] = useState('')
 
-    useEffect(() => {
-        let data = classes.filter(eachClass => {
-            return eachClass.course == stdCourse
-        })
-        console.log(data);
-        setSelectedClass(data)
-    }, [])
-
-
-    return (
-        <select defaultValue={defaultVal}>
-            <option value={defaultVal} disabled >Select {defaultVal}..</option>
+    const loadData = (e) => {
+        deLoad(
             {
-                selectedClass.map((eachClass, index) => {
-                    return <option key={index} value={eachClass[fetchedVal]}>{eachClass[fetchedVal]}</option>
-                })
-                // classes.filter((eachClass, index) => {
-                //     return <option key={index} value={eachClass[fetchedVal]}>{eachClass[fetchedVal]}</option>
-
-                // })
+                selectedBatch,
+                selectedSec : e.target.value
             }
-        </select>
+        )
+    }
+
+    const getClass = (std) => {
+        axios.get(`${api}/get_class/${std.course}`)
+            .then(res => {
+                setClassData(res.data.data)
+            })
+            .catch(err => console.log(err))
+    }
+    return (
+        <div>
+            <select defaultValue={"batchno"} onClick={() => getClass(std)} onChange={(e) => setSelectedBatch(e.target.value)}>
+                <option value="batchno" disabled >Select batch no..</option>
+                {
+                    std.course === classData[0]?.course.toLowerCase() && classData.map((eachClass, index) => {
+                        return <option key={index} value={eachClass.batch}>{eachClass.batch}</option>
+                    })
+                }
+            </select>
+            {
+                (std.course.toLowerCase() === classData[0]?.course.toLowerCase()) &&
+                selectedBatch &&
+                <select defaultValue={"section_name"} onChange={loadData}>
+                    <option value="section_name" disabled>select section ...</option>
+                    {
+                        classData.map((eachClass, index) => {
+                            return <option key={index} value={eachClass.section}>{eachClass.section}</option>
+                        })
+                    }
+                </select>
+            }
+        </div>
     )
 }
 
