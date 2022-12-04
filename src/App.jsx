1cdca356074f3c21c6da2d3fdee1caea6c268b39
obaidmuneer <obaidmuneer@gmail.com>
2022-12-04 15:11:18 +0500

    changed server link

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,13 @@ import Student from './components/student';
 import Attendance from './components/attendance';
 import AddStudent from './components/add_student';
 import AddClass from './components/add_class';
+import QrCode from './components/qrcode';
 
 let api = ''
 if (window.location.protocol === 'http:') {
   api = 'http://localhost:8080'
 } else {
-  api = 'https://helpful-earmuffs-cod.cyclic.app'
+  api = 'https://attendance-management-server-production.up.railway.app'
 }
 
 const App = () => {
@@ -18,16 +19,16 @@ const App = () => {
   return (
     <div className="App">
       <div className="App-container">
-        <h3>Attendance Management System</h3>
+        {/* <h3>Attendance Management System</h3> */}
 
-        <ul>
+        {/* <ul>
           <li><Link to={'/'} >Home</Link></li>
           <li><Link to={'student'} >Student</Link></li>
           <li><Link to={'attendance'} >Attendance</Link></li>
           <li><Link to={'add_class'} >Add Class</Link></li>
-        </ul>
+        </ul> */}
         <Routes>
-          <Route path='/' element={<Home api={api} />} />
+          <Route path='/' element={<QrCode api={api} />} />
           <Route path='student' element={<Student api={api} />} />
           <Route path='add_student' element={<AddStudent api={api} />} />
           <Route path='attendance' element={<Attendance api={api} />} />
