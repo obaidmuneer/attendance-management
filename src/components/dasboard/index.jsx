ea7c309b167de7d45284368b9c55b7df16c17f07
obaidmuneer <obaidmuneer@gmail.com>
2022-12-07 21:23:45 +0500

    send data to server more accurately

--- src/components/dasboard/index.jsx
+++ src/components/dasboard/index.jsx
@@ -7,7 +7,7 @@ import ClassesList from '../classes_list'
 
 const Dashboard = ({ api }) => {
     const [students, setStudents] = useState([])
-    const [data, setData] = useState({})
+    const [data, setData] = useState([])
 
     useEffect(() => {
         axios.get(`${api}/students`)
@@ -16,12 +16,17 @@ const Dashboard = ({ api }) => {
         // eslint-disable-next-line
     }, [])
 
-    const approveStudent = (std) => {
+    const approveStudent = (std, index) => {
+        // console.log(data);
+        const filteredData = data.filter((item) => {
+            return item.uniqKey === index
+        })
+        // console.log(filteredData[0].selectedBatch);
         axios.post(`${api}/enroll_student`, {
             course: std.course,
             roll: std.roll,
-            batch: data.selectedBatch,
-            section: data.selectedSec
+            batch: filteredData[0].selectedBatch,
+            section: filteredData[0].selectedSec
         })
         let studentData = students.filter((eachStd, index) => {
             return eachStd.roll !== std.roll
@@ -36,8 +41,8 @@ const Dashboard = ({ api }) => {
             {
                 students.map((std, index) => {
                     return !std.isClassAssign && <div key={index}>{std.name} applied for {std.course} course
-                        <ClassesList api={api} std={std} deLoad={setData} />
-                        <input type="button" value="Approve" onClick={() => approveStudent(std)} />
+                        <ClassesList api={api} std={std} deLoad={setData} data={data} uniqKey={index} />
+                        <input type="button" value="Approve" onClick={() => approveStudent(std, index)} />
                     </div>
                 })
             }
