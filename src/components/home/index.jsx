bd814edcdf81bba4b9fae89df6cba33503ad2023
obaidmuneer <obaidmuneer@gmail.com>
2022-12-07 21:24:03 +0500

    some changes

--- src/components/home/index.jsx
+++ src/components/home/index.jsx
@@ -1,8 +1,19 @@
+import { Link } from "react-router-dom"
 import Dashboard from "../dasboard"
+
 
 const Home = ({ api }) => {
     return (
         <div>
+            <h3>Attendance Management System</h3>
+
+            <ul>
+                <li><Link  to={'/'} >Mark Attendance</Link></li>
+                <li><Link  to={'/home'} >Home</Link></li>
+                <li><Link  to={'/student'} >Student</Link></li>
+                <li><Link  to={'/attendance'} >Attendance</Link></li>
+                <li><Link  to={'/add_class'} >Add Class</Link></li>
+            </ul>
             <Dashboard api={api} />
         </div>
     )
