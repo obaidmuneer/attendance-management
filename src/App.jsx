eaa223b38a59c4b3629d2461fe3aebfbc6d248eb
obaidmuneer <obaidmuneer55@gmail.com>
2022-12-12 21:45:22 +0500

    added nav

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,13 @@ import Attendance from './components/attendance';
 import AddStudent from './components/add_student';
 import AddClass from './components/add_class';
 import QrCode from './components/qrcode';
+import ResponsiveAppBar from './components/navbar';
 
 let api = ''
 if (window.location.protocol === 'http:') {
   api = 'http://localhost:8080'
 } else {
-  api = 'https://attendance-management-server-production.up.railway.app'
+  api = 'https://attendance-management-server.up.railway.app'
 }
 
 const App = () => {
@@ -19,6 +20,7 @@ const App = () => {
   return (
     <div className="App">
       <div className="App-container">
+        <ResponsiveAppBar />
       
         <Routes>
           <Route path='/' element={<QrCode api={api} />} />
