1b5cc7dcc9f30ad29d16b82feae5ca120335280a
obaidmuneer <obaidmuneer55@gmail.com>
2022-12-13 00:44:56 +0500

    some changes

--- src/components/add_class/index.jsx
+++ src/components/add_class/index.jsx
@@ -1,40 +1,52 @@
-import axios from "axios"
+import axios from "axios";
+import { Formik } from "formik";
 
 const AddClass = ({ api }) => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // just checking i can also do it this way lol
+    let { teacher, classTiming, classSchedule, section, course, batch } =
+      e.target;
+    let data = {
+      teacher: teacher.value,
+      classTiming: classTiming.value,
+      classSchedule: classSchedule.value,
+      section: section.value,
+      course: course.value,
+      batch: batch.value,
+    };
 
-    const handleSubmit = (e) => {
-        e.preventDefault()
-        // just checking i can also do it this way lol 
-        let { teacher, classTiming, classSchedule, section, course, batch } = e.target
-        let data = {
-            teacher: teacher.value,
-            classTiming: classTiming.value,
-            classSchedule: classSchedule.value,
-            section: section.value,
-            course: course.value,
-            batch: batch.value
-        }
+    axios
+      .post(`${api}/add_class`, data)
+      .then((res) => console.log(res.data))
+      .catch((err) => console.log(err));
+  };
+  return (
+    <div>
+      <h3>Add Class</h3>
+      <form onSubmit={handleSubmit}>
+        <span>Teacher :</span>
+        <input type="text" name="teacher" /> <br />
+        <span>Class Timing :</span>
+        <input type="text" name="classTiming" /> <br />
+        <span>Class Schedule :</span>
+        <input type="text" name="classSchedule" />
+        <br />
+        <span>Section :</span>
+        <input type="text" name="section" />
+        <br />
+        <span>Course :</span>
+        <input type="text" name="course" />
+        <br />
+        <span>Batch :</span>
+        <input type="text" name="batch" />
+        <br />
+        <input type="submit" value="Submit" />
+      </form>
 
-        axios.post(`${api}/add_class`, data)
-            .then(res => console.log(res.data))
-            .catch(err => console.log(err))
-    }
-    return (
-        <div>
-            <h3>Add Class</h3>
-            <form onSubmit={handleSubmit} >
-                <span>Teacher :</span><input type="text" name="teacher" /> <br />
-                <span>Class Timing :</span><input type="text" name="classTiming" /> <br />
-                <span>Class Schedule :</span><input type="text" name="classSchedule" /><br />
-                <span>Section :</span><input type="text" name="section" /><br />
-                <span>Course :</span><input type="text" name="course" /><br />
-                <span>Batch :</span><input type="text" name="batch" /><br />
-                <input type="submit" value="Submit" />
+      
+    </div>
+  );
+};
 
-
-            </form>
-        </div>
-    )
-}
-
-export default AddClass
+export default AddClass;
