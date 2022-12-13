d60d64c64128a20235b67f9a4fd08c086abff019
obaidmuneer <obaidmuneer55@gmail.com>
2022-12-13 23:40:33 +0500

    added formik form

--- src/components/add_class/index.jsx
+++ src/components/add_class/index.jsx
@@ -1,7 +1,43 @@
 import axios from "axios";
-import { Formik } from "formik";
+import { useFormik } from "formik";
+import * as yup from "yup";
+import Button from "@mui/material/Button";
+import ModifiedTextField from "../textfield";
+import Box from "@mui/material/Box";
+
+const validationSchema = yup.object({
+  teacher: yup
+    .string("Enter Teacher Name")
+    .required("Teacher's name is required i.e Inzimam Malik"),
+  course: yup
+    .string("Enter Course Name")
+    .min(3)
+    .required("Course name is required i.e WEB, Graphic"),
+  batch: yup.string().required("Batch no is required i.e 1, 2"),
+  section: yup.string().required("Section name is required i.e A, B"),
+  classTiming: yup
+    .string()
+    .required("Class timing is required i.e 9pm to 10pm , 9:00pm to 10:00pm"),
+  classSchedule: yup
+    .string()
+    .required("Section name is required i.e MWF , TTS , Weekdays"),
+});
 
 const AddClass = ({ api }) => {
+  const formik = useFormik({
+    initialValues: {
+      teacher: "",
+      course: "",
+      batch: "",
+      section: "",
+      classTiming: "",
+      classSchedule: "",
+    },
+    validationSchema,
+    onSubmit: (values) => {
+      console.log("Submitted", values);
+    },
+  });
   const handleSubmit = (e) => {
     e.preventDefault();
     // just checking i can also do it this way lol
@@ -22,9 +58,16 @@ const AddClass = ({ api }) => {
       .catch((err) => console.log(err));
   };
   return (
-    <div>
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        // justifyContent: 'center',
+        alignItems: "center",
+      }}
+    >
       <h3>Add Class</h3>
-      <form onSubmit={handleSubmit}>
+      {/* <form onSubmit={handleSubmit}>
         <span>Teacher :</span>
         <input type="text" name="teacher" /> <br />
         <span>Class Timing :</span>
@@ -42,9 +85,46 @@ const AddClass = ({ api }) => {
         <input type="text" name="batch" />
         <br />
         <input type="submit" value="Submit" />
-      </form>
+      </form> */}
 
-      
+      <div>
+        {/* <form onSubmit={formik.handleSubmit}> */}
+        <Box
+        component="form"
+        sx={{
+          "& .MuiTextField-root": {  width: "40ch" },
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          // alignItems: "center",
+        }}
+        // noValidate
+        autoComplete="off"
+        onSubmit={formik.handleSubmit}
+      >
+          <ModifiedTextField str={"teacher"} formik={formik} />
+          <br />
+          <ModifiedTextField str={"course"} formik={formik} />
+          <br />
+          <ModifiedTextField str={"batch"} formik={formik} />
+          <br />
+          <ModifiedTextField str={"section"} formik={formik} />
+          <br />
+          <ModifiedTextField str={"classTiming"} formik={formik} />
+          <br />
+          <ModifiedTextField str={"classSchedule"} formik={formik} />
+          <br />
+          <Button
+            color="primary"
+            sx={{ justifyContent: "center" }}
+            variant="contained"
+            type="submit"
+          >
+            Submit
+          </Button>
+          </Box>
+        {/* </form> */}
+      </div>
     </div>
   );
 };
--- src/components/add_student/index.jsx
+++ src/components/add_student/index.jsx
@@ -1,62 +1,99 @@
-import axios from "axios"
-import { useState } from "react"
-import { useNavigate } from 'react-router-dom'
+import axios from "axios";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { useFormik } from "formik";
+import * as yup from "yup";
+import Button from "@mui/material/Button";
+import ModifiedTextField from "../textfield";
+import Box from "@mui/material/Box";
+import { Typography } from "@mui/material";
+
+const validationSchema = yup.object({
+  name: yup
+    .string("Enter Your Name")
+    .required("Your name is required i.e Obaid Muneer"),
+  fathername: yup
+    .string("Enter Father Name")
+    .required("Father name is required i.e Muneer Hussain"),
+  contact: yup.string().required("Contact no is required i.e +923133853852"),
+  cnic: yup.string().required("CNIC no is required i.e 98754-59887654-5"),
+  course: yup.string().required("Course name is required i.e WEB, AI-Chatbot"),
+});
 
 const AddStudent = ({ api }) => {
-    const [name, setName] = useState('')
-    const [fathername, setFathername] = useState('')
-    const [contact, setContact] = useState('')
-    const [cnic, setCnic] = useState('')
-    const [course, setCourse] = useState('')
-    const [image, setImage] = useState({ preview: '', data: '' })
+  const [name, setName] = useState("");
+  const [fathername, setFathername] = useState("");
+  const [contact, setContact] = useState("");
+  const [cnic, setCnic] = useState("");
+  const [course, setCourse] = useState("");
+  const [image, setImage] = useState({ preview: "", data: "" });
 
-    const navigate = useNavigate()
+  const formik = useFormik({
+    initialValues: {
+      name: "",
+      fathername: "",
+      contact: "",
+      cnic: "",
+      course: "",
+    },
+    validationSchema,
+    onSubmit: (values) => {
+      console.log("Submitted", values);
+    },
+  });
 
-    const handleSubmit = async(e) => {
-        e.preventDefault()
+  const navigate = useNavigate();
 
-        let formData = new FormData();
-        formData.append('pic', image.data)
-        // console.log(formData);
+  const handleSubmit = async (e) => {
+    e.preventDefault();
 
-       const result = await axios.post(`${api}/students`, {
-            name,
-            fathername,
-            contact,
-            cnic,
-            course //make the list to be fethced from server
-        })
-        console.log(result.data.data.roll);
+    let formData = new FormData();
+    formData.append("pic", image.data);
+    // console.log(formData);
 
-        axios.post(`${api}/students/img/${result.data.data.roll}`, formData,
-            {
-                headers: {
-                    'Content-Type': 'multipart/form-data'
-                }
-            })
-            .then((res) => {
-                console.log(res.data);
-            })
-            .catch((err) => {
-                console.log(err);
-            })
+    const result = await axios.post(`${api}/students`, {
+      name,
+      fathername,
+      contact,
+      cnic,
+      course, //make the list to be fethced from server
+    });
+    console.log(result.data.data.roll);
 
-        navigate('/')
+    axios
+      .post(`${api}/students/img/${result.data.data.roll}`, formData, {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      })
+      .then((res) => {
+        console.log(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
 
-    }
+    navigate("/");
+  };
 
-    const handleFileChange = (e) => {
-        const img = {
-            preview: URL.createObjectURL(e.target.files[0]),
-            data: e.target.files[0]
-        }
-        setImage(img)
-    }
+  const handleFileChange = (e) => {
+    const img = {
+      preview: URL.createObjectURL(e.target.files[0]),
+      data: e.target.files[0],
+    };
+    setImage(img);
+  };
 
-    return (
-        <div>
-            <h1>Add Student</h1>
-            <form encType="multipart/form-data" onSubmit={handleSubmit}>
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+      }}
+    >
+      <Typography variant="h4">Add Student</Typography>
+      {/* <form encType="multipart/form-data" onSubmit={handleSubmit}>
                 <span>Name :</span> <input type="text" onChange={(e) => setName(e.target.value)} value={name} /> <br />
                 <span>Father Name :</span> <input type="text" onChange={(e) => setFathername(e.target.value)} value={fathername} /> <br />
                 <span>Contact No:</span> <input type="number" onChange={(e) => setContact(+e.target.value)} value={contact} /> <br />
@@ -70,9 +107,38 @@ const AddStudent = ({ api }) => {
                 </select> <br />
                 <input type="file" name="pic" onChange={handleFileChange} />
                 <input type="submit" value="Add Student" />
-            </form>
-        </div>
-    )
-}
+            </form> */}
+      <Box
+        component="form"
+        encType="multipart/form-data"
+        sx={{
+          "& .MuiTextField-root": { m: 1, width: "40ch" },
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          // alignItems: "center",
+        }}
+        // noValidate
+        autoComplete="off"
+        onSubmit={formik.handleSubmit}
+      >
+        <ModifiedTextField str={"name"} formik={formik} />
+        <ModifiedTextField str={"fathername"} formik={formik} />
+        <ModifiedTextField str={"contact"} formik={formik} />
+        <ModifiedTextField str={"cnic"} formik={formik} />
+        <ModifiedTextField str={"course"} formik={formik} />
+        {/* {"course should be a list to be fethced from server"} */}
+        <Button
+          color="primary"
+          sx={{ justifyContent: "center" }}
+          variant="contained"
+          type="submit"
+        >
+          Submit
+        </Button>
+      </Box>
+    </div>
+  );
+};
 
-export default AddStudent
+export default AddStudent;
