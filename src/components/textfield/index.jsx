57f81ba85143e3cbd0bd9d491d31c3416d7ca812
obaidmuneer <obaidmuneer55@gmail.com>
2022-12-13 23:40:53 +0500

    textfield modified

---
+++ src/components/textfield/index.jsx
@@ -0,0 +1,22 @@
+import TextField from "@mui/material/TextField";
+
+const ModifiedTextField = ({ str, formik }) => {
+  return (
+    <TextField
+      id={str}
+      name={str}
+      label={str.charAt(0).toUpperCase() + str.slice(1)}
+    //   margin="dense"
+      value={formik.values[str]}
+      InputProps={{ sx: { height: 50 } }}
+      sx={{
+        width: 300,
+      }}
+      onChange={formik.handleChange}
+      error={formik.touched[str] && Boolean(formik.errors[str])}
+      helperText={formik.touched[str] && formik.errors[str]}
+    />
+  );
+};
+
+export default ModifiedTextField;
