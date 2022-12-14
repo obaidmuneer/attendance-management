f26d330eecd6e779e1d7df8edcb788b2a1bd1d7b
obaidmuneer <obaidmuneer55@gmail.com>
2022-12-14 23:53:52 +0500

    improved ui

--- src/components/student_card/index.jsx
+++ src/components/student_card/index.jsx
@@ -3,29 +3,32 @@ import CardContent from "@mui/material/CardContent";
 import Typography from "@mui/material/Typography";
 import Avatar from "@mui/material/Avatar";
 import Stack from "@mui/material/Stack";
+import { CardActionArea } from "@mui/material";
 
 export default function StudentCard({ student }) {
   return (
-    <Card sx={{ maxWidth: 275 }}>
-      <Stack direction="row" sx={{ alignItems: "center" }} spacing={2}>
-        <CardContent>
-          <Typography variant="body1" gutterBottom>
-            Student Card
-          </Typography>
-          <Typography variant="body2" component="div">
-            Name : {student?.name}
-          </Typography>
+    <CardActionArea sx={{width: 275}} >
+      <Card sx={{ maxWidth: 275 }}>
+        <Stack direction="row" sx={{ alignItems: "center" }} spacing={2}>
+          <CardContent>
+            <Typography variant="body1" gutterBottom>
+              Student Card
+            </Typography>
+            <Typography variant="body2" component="div">
+              Name : {student?.name}
+            </Typography>
 
-          <Typography variant="body2">Course: {student?.course}</Typography>
-          <Typography variant="body2">Roll: {student?.roll}</Typography>
-        </CardContent>
-        <Avatar
-          sx={{ width: 70, height: 70 }}
-          alt="profile"
-          src={student?.picture?.link}
-        />
-        {/* {Add alpha photo if avatar not found} */}
-      </Stack>
-    </Card>
+            <Typography variant="body2">Course: {student?.course}</Typography>
+            <Typography variant="body2">Roll: {student?.roll}</Typography>
+          </CardContent>
+          <Avatar
+            sx={{ width: 70, height: 70 }}
+            alt="profile"
+            src={student?.picture?.link}
+          />
+          {/* {Add alpha photo if avatar not found} */}
+        </Stack>
+      </Card>
+    </CardActionArea>
   );
 }
