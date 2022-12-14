cdba1b4975f25895c1f62174ca31ada527ae28a4
obaidmuneer <obaidmuneer55@gmail.com>
2022-12-14 23:52:42 +0500

    improved uix

--- src/components/textfield/index.jsx
+++ src/components/textfield/index.jsx
@@ -1,17 +1,19 @@
 import TextField from "@mui/material/TextField";
 
-const ModifiedTextField = ({ str, formik }) => {
+const ModifiedTextField = ({ str, type, placeHelper, formik }) => {
   return (
     <TextField
       id={str}
       name={str}
       label={str.charAt(0).toUpperCase() + str.slice(1)}
-    //   margin="dense"
+      //   margin="dense"
       value={formik.values[str]}
       InputProps={{ sx: { height: 50 } }}
       sx={{
         width: 300,
       }}
+      placeholder={placeHelper}
+      type={type || "text"}
       onChange={formik.handleChange}
       error={formik.touched[str] && Boolean(formik.errors[str])}
       helperText={formik.touched[str] && formik.errors[str]}
