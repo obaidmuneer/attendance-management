02c348c08e736f562f4939f5169cfec84f5891d4
obaidmuneer <obaidmuneer55@gmail.com>
2022-12-15 23:14:22 +0500

    added darkmode btn / context linked

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,53 @@
-import { Routes, Route } from 'react-router-dom'
-import './App.css';
-import Home from './components/home';
-import Student from './components/student';
-import Attendance from './components/attendance';
-import AddStudent from './components/add_student';
-import AddClass from './components/add_class';
-import QrCode from './components/qrcode';
-import ResponsiveAppBar from './components/navbar';
+import { Routes, Route } from "react-router-dom";
+import "./App.css";
+import Home from "./components/home";
+import Student from "./components/student";
+import Attendance from "./components/attendance";
+import AddStudent from "./components/add_student";
+import AddClass from "./components/add_class";
+import QrCode from "./components/qrcode";
+import ResponsiveAppBar from "./components/navbar";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import { useContext } from "react";
+import { GlobalContext } from "./context/context";
 
-let api = ''
-if (window.location.protocol === 'http:') {
-  api = 'http://localhost:8080'
+let api = "";
+if (window.location.protocol === "http:") {
+  api = "http://localhost:8080";
 } else {
-  api = 'https://attendance-management-server.up.railway.app'
+  api = "https://attendance-management-server.up.railway.app";
 }
 
 const App = () => {
+  let { state, dispatch } = useContext(GlobalContext);
+  console.log(state);
+  const darkTheme = createTheme({
+    palette: {
+      mode: state.theme,
+    },
+  });
 
   return (
-    <div className="App">
-      <div className="App-container">
-        <ResponsiveAppBar />
-      
-        <Routes>
-          <Route path='/' element={<QrCode api={api} />} />
-          <Route path='home' element={<Home api={api} />} />
-          <Route path='student' element={<Student api={api} />} />
-          <Route path='add_student' element={<AddStudent api={api} />} />
-          <Route path='attendance' element={<Attendance api={api} />} />
-          <Route path='add_class' element={<AddClass api={api} />} />
-        </Routes>
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+
+      <div className="App">
+        <div className="App-container">
+          <ResponsiveAppBar />
+
+          <Routes>
+            <Route path="/" element={<QrCode api={api} />} />
+            <Route path="home" element={<Home api={api} />} />
+            <Route path="student" element={<Student api={api} />} />
+            <Route path="add_student" element={<AddStudent api={api} />} />
+            <Route path="attendance" element={<Attendance api={api} />} />
+            <Route path="add_class" element={<AddClass api={api} />} />
+          </Routes>
+        </div>
       </div>
-    </div>
+    </ThemeProvider>
   );
-}
+};
 
 export default App;
