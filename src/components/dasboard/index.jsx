ff367a17794544bee2c8393fea3871dfbbfc2592
obaidmuneer <obaidmuneer55@gmail.com>
2022-12-17 00:02:53 +0500

    updated dashbord with top 5 std carousel/card

--- src/components/dasboard/index.jsx
+++ src/components/dasboard/index.jsx
@@ -1,18 +1,33 @@
 import { useEffect, useState } from 'react'
 import axios from 'axios'
-import { Link } from 'react-router-dom'
 import ClassesList from '../classes_list'
-
+import StudentCard from '../student_card'
+import Stack from "@mui/material/Stack";
+import Carousel from 'react-material-ui-carousel'
+import { Typography } from '@mui/material'
+import Box from '@mui/material/Box';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemText from '@mui/material/ListItemText';
+import Divider from '@mui/material/Divider';
 
 
 const Dashboard = ({ api }) => {
     const [students, setStudents] = useState([])
+    const [topStudents, setTopStudents] = useState([])
+    const [checkStudents, setCheckStudents] = useState([])
     const [data, setData] = useState([])
 
     useEffect(() => {
         axios.get(`${api}/students`)
             .then(res => setStudents(res.data.data))
         // console.log(students);
+        axios.post(`${api}/top_check_std`)
+            .then(res => {
+                setTopStudents(res.data.topStudents)
+                setCheckStudents(res.data.checkStudents)
+            })
         // eslint-disable-next-line
     }, [])
 
@@ -35,16 +50,67 @@ const Dashboard = ({ api }) => {
     }
 
     return (
-        <div>
+        <div style={{ padding: '1%' }} >
             <h3>Dashbord</h3>
-            {
-                students.map((std, index) => {
-                    return !std.isClassAssign && <div key={index}>{std.name} applied for {std.course} course
-                        <ClassesList api={api} std={std} deLoad={setData} data={data} uniqKey={index} />
-                        <input type="button" value="Approve" onClick={() => approveStudent(std, index)} />
-                    </div>
-                })
-            }
+            {/* <EnrollStudents api={api} deLoad={setData} data={data} students={students} /> */}
+
+            <Box sx={{ width: '100%', bgcolor: 'background.paper' }}>
+                <List >
+                    {
+                        students.map((std, index) => {
+                            return !std.isClassAssign && <div key={index}>
+                                <ListItem disableTouchRipple={true} disablePadding >
+                                    <ListItemButton>
+                                        <ListItemText primary={`${std.name} applied for ${std.course} course`} />
+                                        <ClassesList api={api} std={std} deLoad={setData} data={data} uniqKey={index} />
+                                        <input type="button" value="Approve" onClick={() => approveStudent(std, index)} />
+                                    </ListItemButton>
+                                </ListItem>
+                                <Divider />
+
+                            </div>
+                        })
+                    }
+
+                </List>
+            </Box>
+
+
+
+            <Stack direction="row" sx={{ justifyContent: "space-around" }} spacing={2}>
+                <div className='carousel-container' >
+                    <Typography variant='h5'> Top Students by Attendance</Typography>
+                    <Carousel
+                        indicators={false}
+                        height={150}
+                    // sx={{ width: '25%' }}
+                    >
+
+                        {
+                            topStudents.map(student => {
+                                return <StudentCard key={student.roll} student={student} />
+                            })
+                        }
+                    </Carousel>
+                </div>
+
+                <div>
+                    <Typography variant='h5'> Attension needed Students</Typography>
+                    <Carousel
+                        indicators={false}
+                        height={150}
+                    // sx={{ width: '25%' }}
+                    >
+
+                        {
+                            checkStudents.map(student => {
+                                return <StudentCard key={student.roll} student={student} />
+                            })
+                        }
+                    </Carousel>
+                </div>
+            </Stack>
+
         </div>
 
     )
