ed57de18e12de8fd3b4d7c48bd7cd492f0c775e6
obaidmuneer <obaidmuneer55@gmail.com>
2022-12-18 13:36:52 +0500

    mui select

---
+++ src/components/select_section/index.jsx
@@ -0,0 +1,41 @@
+import { useState } from 'react'
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+
+export default function SelectSection({ classData, selectedBatch, deLoad, data, uniqKey }) {
+    const [section, setSection] = useState('')
+
+    const handleChange = (e) => {
+        setSection(e.target.value)
+        deLoad(
+            [...data, {
+                selectedBatch,
+                selectedSec: section,
+                uniqKey
+            }]
+        )
+    }
+
+    return (
+        <FormControl sx={{ minWidth: 150 }}>
+            <InputLabel id="section-select-label">Select Section</InputLabel>
+            <Select
+                labelId="section-select-label"
+                id="section-select"
+                value={section}
+                label="Select Section"
+                onChange={handleChange}
+            >
+                {
+                    classData.map((eachClass, index) => {
+                        return eachClass.batch === selectedBatch &&
+                            <MenuItem key={index} value={eachClass.section}>{eachClass.section}</MenuItem>
+                    })
+                }
+
+            </Select>
+        </FormControl>
+    );
+}
