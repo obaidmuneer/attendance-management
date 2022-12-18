a1ea4fe2979cbfa31877bb95fc6db2086d8c6603
obaidmuneer <obaidmuneer55@gmail.com>
2022-12-18 13:37:44 +0500

    mui select sec

--- src/components/classes_list/index.jsx
+++ src/components/classes_list/index.jsx
@@ -1,64 +1,26 @@
-import axios from "axios"
 import { useState } from "react"
+import Box from '@mui/material/Box';
+import AsyncSelectBatch from "../async_select_batch"
+import SelectSection from "../select_section"
+import Stack from "@mui/material/Stack";
 
 
-const ClassesList = ({ api, std, deLoad, data ,uniqKey }) => {
+const ClassesList = ({ api, std, deLoad, data, uniqKey }) => {
     const [classData, setClassData] = useState([])
     const [selectedBatch, setSelectedBatch] = useState('')
-    const [batchList, setBatchList] = useState([])
 
-    const loadData = (e) => {
-        deLoad(
-            [...data, {
-                selectedBatch,
-                selectedSec: e.target.value,
-                uniqKey
-            }]
-        )
-    }
-
-    const getClass = (std) => {
-        axios.get(`${api}/get_class/${std.course}`)
-            .then(res => {
-                setClassData(res.data.data)
-                uniq(res.data.data)
-            })
-            .catch(err => console.log(err))
-    }
-
-    const uniq = (data) => {
-        const a = data.map((item) => {
-            return item.batch
-        })
-        const uniqArr = [...new Set(a)];
-        setBatchList(uniqArr)
-        // api could be added 
-    }
     return (
-        <span>
-            {/* <button onClick={uniq} >Click me</button> */}
-
-            <select defaultValue={"batchno"} onClick={() => getClass(std)} onChange={(e) => setSelectedBatch(e.target.value)}>
-                <option value="batchno" disabled >Select batch no..</option>
+        <Box sx={{ minWidth: 300, mr:1 }} >
+            <Stack spacing={1} direction="row">
+                <AsyncSelectBatch selectbatch={setSelectedBatch} selectClass={setClassData} std={std} api={api} />
                 {
-                    std.course === classData[0]?.course.toLowerCase() && batchList.map((eachClass, index) => {
-                        return <option key={index} value={eachClass}>{eachClass}</option>
-                    })
+                    (std.course.toLowerCase() === classData[0]?.course.toLowerCase()) &&
+                    selectedBatch &&
+                    <SelectSection classData={classData} selectedBatch={selectedBatch} data={data} uniqKey={uniqKey} deLoad={deLoad} />
                 }
-            </select>
-            {
-                (std.course.toLowerCase() === classData[0]?.course.toLowerCase()) &&
-                selectedBatch &&
-                <select defaultValue={"section_name"} onChange={loadData}>
-                    <option value="section_name" disabled>select section ...</option>
-                    {
-                        classData.map((eachClass, index) => {
-                            return eachClass.batch === selectedBatch && <option key={index} value={eachClass.section}>{eachClass.section}</option>
-                        })
-                    }
-                </select>
-            }
-        </span>
+            </Stack>
+        </Box>
+
     )
 }
 
