08dde0f14bf9e74f77271b9575bf447b388a4a8c
obaidmuneer <obaidmuneer55@gmail.com>
2023-01-03 22:21:23 +0500

    MMenu ui comp

---
+++ src/ui-components/mmenu/index.jsx
@@ -0,0 +1,67 @@
+import * as React from 'react';
+import { Link } from "react-router-dom";
+
+import Button from '@mui/material/Button';
+import Menu from "@mui/material/Menu";
+import MenuItem from '@mui/material/MenuItem';
+import Stack from '@mui/material/Stack';
+import Typography from "@mui/material/Typography";
+
+export default function MMenu({ title, pages, nav, closeMenu }) {
+    const [anchorEl, setAnchorEl] = React.useState(null);
+
+    const handleOpenMenu = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+
+    const handleCloseMenu = () => {
+        setAnchorEl(null);
+        nav && closeMenu();
+    };
+
+    return (
+        <Stack >
+            {
+                nav ?
+                    <MenuItem onClick={handleOpenMenu}>{title}</MenuItem>
+                    :
+                    <Button
+                        onClick={handleOpenMenu}
+                        sx={{ my: 2, color: "white", display: "block" }}
+                    >
+                        {title}
+                    </Button>
+            }
+            <Menu
+                id="simple-menu"
+                anchorEl={anchorEl}
+                keepMounted
+                open={Boolean(anchorEl)}
+                onClose={handleCloseMenu}
+
+                anchorOrigin={{
+                    vertical: nav ? 'top' : 'bottom',
+                    horizontal: 'right',
+                }}
+                transformOrigin={{
+                    vertical: 'top',
+                    horizontal: nav ? 'left' : 'center',
+                }}
+
+            >
+                {pages.map((page, index) => (
+                    <MenuItem key={index} onClick={handleCloseMenu}>
+                        <Typography textAlign="center">
+                            <Link
+                                style={{ textDecoration: "inherit", color: "inherit" }}
+                                to={page.path}
+                            >
+                                {page.title}
+                            </Link>
+                        </Typography>
+                    </MenuItem>
+                ))}
+            </Menu>
+        </Stack>
+    );
+}
