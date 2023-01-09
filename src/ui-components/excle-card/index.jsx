b04f549057e186cfc54c725ebeb77c2283d1df7f
obaidmuneer <obaidmuneer55@gmail.com>
2023-01-09 19:50:25 +0500

    ExcleMediaCard

---
+++ src/ui-components/excle-card/index.jsx
@@ -0,0 +1,36 @@
+import * as React from 'react';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import { Stack } from '@mui/system';
+
+export default function ExcleMediaCard({ img, givenWidth }) {
+    return (
+        <Stack alignItems={'center'} >
+            <Card raised sx={{ maxWidth: givenWidth || 550 }}>
+                <CardMedia
+                    component="img"
+                    alt="guide pic"
+                    height="150"
+                    image={img}
+
+                />
+                <CardContent>
+                    {/* <Typography gutterBottom variant="h5" component="div">
+                    Guide
+                </Typography> */}
+                    <Typography variant="body2" color="text.secondary">
+                        Your Excle file should be as shown in image
+                    </Typography>
+                </CardContent>
+                {/* <CardActions>
+                <Button size="small">Ok</Button>
+                <Button size="small">Dont Show Again</Button>
+            </CardActions> */}
+            </Card>
+        </Stack>
+    );
+}
